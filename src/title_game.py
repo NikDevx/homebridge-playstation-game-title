e9d66dab27dc662323778f10f0e6b21c2ca41699
NikDevx <andrievskiy_nikita@yahoo.com>
2024-02-11 16:45:27 +0200

    added errors catch and show them instead of game title

--- src/title_game.py
+++ src/title_game.py
@@ -2,9 +2,16 @@ import json
 import sys
 
 from psnawp_api import PSNAWP
+from psnawp_api.core import psnawp_exceptions
 
-psnawp = PSNAWP(sys.argv[1])
-client = psnawp.me()
+try:
+    PSNAWP(sys.argv[1])
+except psnawp_exceptions.PSNAWPAuthenticationError as e:
+    psnawp = False
+    psnawp_code = "Your npsso code has expired or is incorrect. Please generate a new code!"
+else:
+    psnawp = PSNAWP(sys.argv[1])
+    client = psnawp.me()
 
 
 def user_status():
@@ -16,15 +23,21 @@ def user_status():
     game_id = "CUSAXXXXXX"
     game_title = "Loading..."
 
-    for user in accounts_ids:
-        user_name = psnawp.user(account_id=user).account_id
-        user_info = psnawp.user(account_id=user).get_presence()
-        if user_info["basicPresence"]["primaryPlatformInfo"]["onlineStatus"] == "online" and user == user_name:
-            if "gameTitleInfoList" in user_info["basicPresence"]:
-                game_id = user_info["basicPresence"]["gameTitleInfoList"][0]["npTitleId"]
-                game_title = user_info["basicPresence"]["gameTitleInfoList"][0]["titleName"]
-            else:
-                game_title = "Not playing"
+    if psnawp:
+        for user in accounts_ids:
+            user_name = psnawp.user(account_id=user).account_id
+            user_info = psnawp.user(account_id=user).get_presence()
+            if user_info["basicPresence"]["primaryPlatformInfo"]["onlineStatus"] == "online" and user == user_name:
+                if "gameTitleInfoList" in user_info["basicPresence"]:
+                    game_id = user_info["basicPresence"]["gameTitleInfoList"][0]["npTitleId"]
+                    game_title = user_info["basicPresence"]["gameTitleInfoList"][0]["titleName"]
+                else:
+                    game_title = "Not playing"
+    else:
+        if psnawp_code == "Your npsso code has expired or is incorrect. Please generate a new code!":
+            game_title = "NPSSO code has expired or incorrect! Replace it!"
+        else:
+            game_title = "Something went wrong while authenticating!"
 
     return game_title
 
